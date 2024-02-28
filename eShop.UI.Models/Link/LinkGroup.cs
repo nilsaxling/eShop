53e99097270d3e3238e25b22ad0b0b95ec03d71d
nilsaxling <nilsaxling@gmail.com>
2024-02-28 11:46:09 +0100

    innan CategoryHttpClient

---
+++ eShop.UI.Models/Link/LinkGroup.cs
@@ -0,0 +1,7 @@
+﻿namespace eShop.UI.Models.Link;
+
+public class LinkGroup
+{
+    public string Name { get; set; } = string.Empty;
+    public List<LinkOption> LinkOptions { get; set; } = [];
+}
---
+++ eShop.UI.Models/Link/LinkOption.cs
@@ -0,0 +1,8 @@
+﻿namespace eShop.UI.Models.Link;
+
+public class LinkOption
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public bool IsSelected { get; set; }
+}
